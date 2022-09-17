86e2bc3d2d41dd95b0adbcb533804d4d4bbcbb84
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-17 20:26:10 -0300

    creating home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+import styles from './Home.module.css'
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>Cost</span></h1>
+            <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto"></LinkButton>
+            <img src={savings} alt="Cost"></img>
+
+        </section>
+    )
 }
 
 export default Home
