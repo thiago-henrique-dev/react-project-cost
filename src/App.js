c0895bd8ce9d415735c1bb6643026aa2529fd82d
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-17 19:42:43 -0300

    navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects';
+
 
 function App() {
   return (
-    <p>Cost</p>
+    <div>
+        <Router>
+          <Navbar/>
+          <Container customClass="min-height">
+          <Routes>
+            <Route path='/' exact='' element={<Home/>}></Route>
+            <Route path='/company' element={<Projects/>}></Route>
+            <Route path='/company' element={<Company/>}></Route>
+            <Route path='/contact' element={<Contact/>}></Route>
+            <Route path='/newproject' element={<NewProject/>}></Route>
+          </Routes>
+          </Container>
+        </Router>
+        <Footer/>
+
+    </div>
   );
 }
 
