ec8367126d1a079d1dec4cbb66e59e9a9a1bbf1d
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-19 09:20:24 -0300

    Conectando com a API e request categorias

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,8 +2,26 @@ import styles from './ProjectForm.module.css'
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
+import { useState, useEffect} from 'react'
 
 function ProjectForm( { btnText }) {
+  const [ categories, setCategories ] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+      mathod: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then((resp) => resp.json())
+      .then((data) => {
+        setCategories(data)
+      })
+      .catch((err) => console.log(err))
+
+  }, [])
+  
+
   return (
     <form className={styles.form_control}>
         <Input 
@@ -15,11 +33,12 @@ function ProjectForm( { btnText }) {
               type="number" 
               text="Orçamento do projeto"
               name="name" 
-              placeholder="Insira o nome do projeto"/>
+              placeholder="Insira o orçamento do projeto"/>
     
      <Select 
             name="category_id" 
-            text="Selecione a categoria"/>
+            text="Selecione a categoria"
+            options={categories}/>
       <SubmitButton text={btnText}/>
     </form>
   );
