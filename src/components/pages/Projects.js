f6b6b941ae7172cadc2f866ca970da50cccd11bd
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-09-20 16:43:58 -0300

    Criando componente de Loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,23 +3,30 @@ import { useLocation } from 'react-router-dom'
 import styles from './Project.module.css'
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
 function Projects(){
+
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     useEffect(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        }).then(resp => resp.json())
-        .then(data => {
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
+        setTimeout(() => {
+
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }).then(resp => resp.json())
+            .then(data => {
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => console.log(err))
+        }, 1000)
     },[])
 
     const location = useLocation()
@@ -49,6 +56,12 @@ function Projects(){
                    
                         />
                 )}
+                {!removeLoading && <Loading/>}
+                {removeLoading && projects.length === 0
+                    (
+                        <p>Não há projetos cadastrados!</p>
+                    )
+                }
             </Container>
         </div>
     )
